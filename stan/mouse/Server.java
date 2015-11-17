4d11a92baa2e8b37ebebf34dd853fc15e916cc20
Anton <a.grechushnikov@octopod.com>
2015-11-17 05:02:12 +0300

    init server + try connect droid client with udp socket

---
+++ stan/mouse/Server.java
@@ -0,0 +1,33 @@
+package stan.mouse;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class Server
+{
+    static final int serverPort = 9876;
+    
+    public static void main(String[] args) throws SocketException, IOException
+    {
+        DatagramSocket serverSocket = new DatagramSocket(serverPort);
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+        while(true)
+        {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            String sentence = new String( receivePacket.getData());
+            System.out.println("RECEIVED: " + sentence);
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            String capitalizedSentence = sentence.toUpperCase();
+            sendData = capitalizedSentence.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            serverSocket.send(sendPacket);
+        }
+    }
+    
+}
